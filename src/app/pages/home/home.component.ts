1d6c047ba9a63a07de68fcf69f0c0daf7527eb5e
Javier Sancho <sanchopej@gmail.com>
2025-03-25 13:48:04 +0100

    validadores hechos, falta el de la url

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { PublicacionesService } from '../../services/publicaciones.service';
 import { RouterLink } from '@angular/router';
 
+
 @Component({
   selector: 'app-home',
   imports: [RouterLink],
--- src/app/pages/new/new.component.ts
+++ src/app/pages/new/new.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl, PatternValidator, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new.component.html',
   styleUrl: './new.component.css'
 })
 export class NewComponent {
 
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      id: new FormControl(null),
+      titulo: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      texto: new FormControl('', [Validators.required, Validators.maxLength(500), Validators.minLength(140)]),
+      autor: new FormControl('', Validators.required),
+      imagen: new FormControl('', Validators.required),
+      fecha: new FormControl(null, Validators.required),
+      categoria: new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Publicación añadida', this.form.value);
+    } else {
+      console.log('Publicación no valida');
+    }
+  }
+  checkError(field: string, validator: string): boolean | undefined {
+    return this.form.get(field)?.hasError(validator) && this.form.get(field)?.touched;
+  }
 }
