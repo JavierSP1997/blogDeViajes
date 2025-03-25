ec8d436c8122824747d2f07c42ac0ca67035fe7b
Javier Sancho <sanchopej@gmail.com>
2025-03-25 09:59:19 +0100

    Output categoria seleccionada creado

--- src/app/components/selector-categoria/selector-categoria.component.ts
+++ src/app/components/selector-categoria/selector-categoria.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-selector-categoria',
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class SelectorCategoriaComponent {
 
+  @Output() categoriaSeleccionada = new EventEmitter<string>();
+
+  onCategoriaSeleccionada(event: Event): void {
+    const categoria = (event.target as HTMLSelectElement).value;
+    this.categoriaSeleccionada.emit(categoria)
+  }
 }
